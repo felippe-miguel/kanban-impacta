e099c41bb7f91e89645f3c63c50ee1f2c884af67
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-08-23 18:47:38 +0000

    Adiciona controlador e recurso Board, e define rotas API para gerenciamento de boards

---
+++ app/app/Http/Controllers/BoardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BoardResource;
+use App\Models\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    public function index()
+    {
+        $boards = Board::all();
+        return BoardResource::collection($boards);
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(['message' => 'Board created']);
+    }
+
+    public function show($id)
+    {
+        return response()->json(['message' => "Showing board with ID: $id"]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json(['message' => "Updating board with ID: $id"]);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json(['message' => "Deleting board with ID: $id"]);
+    }
+}
