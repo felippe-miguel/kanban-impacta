01905c3625536ebf7931d7d6fb84b1b4cbc1e1f2
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-09-10 02:18:25 +0000

    Add routes for redirecting to boards index and managing columns

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('boards.index');
+    });
 	Route::resource('boards', App\Http\Controllers\BoardController::class);
+    Route::post('boards/{board}/columns', [App\Http\Controllers\ColumnController::class, 'store'])->name('columns.store');
+    Route::delete('boards/{board}/columns/{column}', [App\Http\Controllers\ColumnController::class, 'destroy'])->name('columns.destroy');
 });
