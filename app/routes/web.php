dcdfd41844bdef5e51f7a645bb37fe9a5e95881d
Felippe Miguel <felippe.miguel.silva@gmail.com>
2025-09-11 00:22:29 +0000

    Add routes for managing cards in the board resource

--- app/routes/web.php
+++ app/routes/web.php
@@ -9,4 +9,7 @@ Route::middleware(['web'])->group(function () {
 	Route::resource('boards', App\Http\Controllers\BoardController::class);
     Route::post('boards/{board}/columns', [App\Http\Controllers\ColumnController::class, 'store'])->name('columns.store');
     Route::delete('boards/{board}/columns/{column}', [App\Http\Controllers\ColumnController::class, 'destroy'])->name('columns.destroy');
+
+    Route::post('boards/{board}/cards', [App\Http\Controllers\CardController::class, 'store'])->name('cards.store');
+    Route::delete('boards/{board}/cards/{column}', [App\Http\Controllers\CardController::class, 'destroy'])->name('cards.destroy');
 });
